94644a3c09f0698b8d4ae62dcdc377a2b816fd1b
hiroto0927 <hiroto.0927.123@gmail.com>
2023-12-08 17:08:27 +0000

    echoが動くことを確認

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// インスタンスを作成
+	e := echo.New()
+
+	// ミドルウェアを設定
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// ルートを設定
+	e.GET("/", hello) // ローカル環境の場合、http://localhost:1323/ にGETアクセスされるとhelloハンドラーを実行する
+
+	// サーバーをポート番号1323で起動
+	e.Logger.Fatal(e.Start(":4000"))
+}
+
+// ハンドラーを定義
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "HelloWorld!")
 }
