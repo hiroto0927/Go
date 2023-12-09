f6c9e3f932ecdf286a2b3faa2ed094a4d27593bb
hiroto0927 <hiroto.0927.123@gmail.com>
2023-12-09 18:01:43 +0000

    Goの環境を構築完了

--- main.go
+++ main.go
@@ -8,21 +8,18 @@ import (
 )
 
 func main() {
-	// インスタンスを作成
+
 	e := echo.New()
 
-	// ミドルウェアを設定
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	// ルートを設定
-	e.GET("/", hello) // ローカル環境の場合、http://localhost:1323/ にGETアクセスされるとhelloハンドラーを実行する
+	e.GET("/", hello)
 
-	// サーバーをポート番号1323で起動
 	e.Logger.Fatal(e.Start(":4000"))
 }
 
 // ハンドラーを定義
 func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+	return c.String(http.StatusOK, "Hello-World!")
 }
